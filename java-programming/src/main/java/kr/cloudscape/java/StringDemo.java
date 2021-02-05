27eae24ac4618f82c6bfaab8d3760e05957cc255
darkchoco <darkchoco.ice@gmail.com>
2021-02-05 18:29:48 +0100

    java-programming: Codes arrangement finished.

---
+++ java-programming/src/main/java/kr/cloudscape/java/StringDemo.java
@@ -0,0 +1,11 @@
+package kr.cloudscape.java;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse().toString());
+    }
+}
